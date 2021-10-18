029dcef81c073599a84c8d1c5ba7ece5ffd03ca8
f3bandit <f3bandit@gmail.com>
2021-10-18 15:54:32 -0400

    Delete sdsupp.h

--- source/sdsupp.h
+++
@@ -1,9 +0,0 @@
-#define MCSAVES "MCBACKUP"
-
-int SDSaveMCImage();
-int SDLoadMCImage(char *sdfilename);
-int SDLoadMCImageHeader(char *sdfilename);
-int SDLoadCardImageHeader(char *sdfilename);
-int isdir_sd(char *path);
-int SDGetFileList(int mode);
-bool file_exists(const char * filename);
