186b27d5204c637197dd4c4ece8bbc392416918b
f3bandit <f3bandit@gmail.com>
2021-10-18 15:53:38 -0400

    Delete bitmap.h

--- source/bitmap.h
+++
@@ -1,9 +0,0 @@
-/*** 2D Video Globals ***/
-extern GXRModeObj *vmode; /*** Graphics Mode Object ***/
-extern u32 *xfb[2]; /*** Framebuffers ***/
-extern int whichfb; /*** Frame buffer toggle ***/
-u32 CvtRGB(u8 r1, u8 g1, u8 b1, u8 r2, u8 g2, u8 b2);
-u32 ShowBMP (u8 *bmpfile);
-void ShowBanner ();
-void ShowIcon   ();
-void ClearScreen();
